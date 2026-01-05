fc0ce4179a52a4b8bc58c5d4813093a3200dd7b4
SunnySharma33 <sunnysharma12096@gmail.com>
2026-01-06 01:01:09 +0530

    Add files via upload

---
+++ Email Reply Assistant-Backend/src/main/java/com/email/writer/EmailController.java
@@ -0,0 +1,20 @@
+package com.email.writer;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/email")
+@AllArgsConstructor
+public class EmailController {
+    private final EmailService eService;
+    @PostMapping("/generate")
+    public ResponseEntity<String> generateEmail(@RequestBody EmailRequest emailRequest) {
+      String response = EmailService.generateEmailReply(emailRequest);
+        return ResponseEntity.ok(response);
+    }
+}
